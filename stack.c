2621da09f7b89f98a71443c49a46346471f3f521
belb2 <nathanbiru21@gmail.com>
2022-12-19 14:21:29 +0300

    new commit

--- stack.c
+++ stack.c
@@ -1,70 +1,13 @@
 #include "monty.h"
-#include <string.h>
-
-void free_stack
 /**
- *free stack - frees a stack_t stack
- *@stack: a pointer to the top (stack) 
- *
- */
-void free_stack(stack_t **stack)
+ * f_stack - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_stack(stack_t **head, unsigned int counter)
 {
-	stack_t *tmp = *stack;
-	while (*stack)
-	{
-		temp = (*stack)->next;
-		free(*stack);
-		*stack = tmp;
-	}
-}
-/**
- *init_stack - initializes a stack_t stack with beginning and ending que nodes
- *@stack: a pointer to the top (stack) 
- *
- */
-void free_stack(stack_t **stack)
-{
-	stack_t *tmp = *stack;
-	while (*stack)
-	{
-		temp = (*stack)->next;
-		free(*stack);
-		*stack = tmp;
-	}
-}
-
-/**
- *init_stack - initializes a stack_t stack with beginning and ending que nodes
- *@stack: a pointer to the top (stack) 
- *
- * return: if an error occurrs -EXIT_FAILURE otherwise -EXIT_SUCCESS
- */
-void init_stack(stack_t **stack)
-{
-	stack_t *s;
-	s = malloc(sizeof(stack_t));
-	if (s == NULL )
-		return (malloc_error());
-
-	s->n = STACK;
-	s->prev = NULL;
-	s->next = NULL;
-
-	*stack = s;
-	return (EXIT_SUCCESS);
-}
-
-/**
- *check_mode - checks if a stack_t linked llist id in stack or que mode
- *@stack: a pointer to the top (stack) 
- *
- * return: if stack_t in stack mode - STACK(0)
- *         if the stack_t is in queue mode - QUEUE(1) otherwise 2.
- */
-void check_mode(stack_t *stack)
-{
-	if (stack->n == STACK)
-		return (STACK);
-	elseif (stack ->n == QUEUE)
-		return(2);
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
 }
